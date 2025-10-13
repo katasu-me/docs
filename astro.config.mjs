1d6998c780d072de1984f1c6aa1f312cc8ba6209
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-10-13 21:40:46 +0900

    🎉 あい

---
+++ astro.config.mjs
@@ -0,0 +1,34 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "katasu.me docs",
+      customCss: [
+        "./src/styles/custom.css",
+      ],
+      social: [{
+        icon: "github",
+        label: "GitHub",
+        href: "https://github.com/katasu-me",
+      }],
+      sidebar: [
+        {
+          label: "katasu.meについて",
+          autogenerate: { directory: "info" },
+        },
+        {
+          label: "更新履歴",
+          autogenerate: { directory: "news" },
+        },
+        {
+          label: "その他",
+          autogenerate: { directory: "others" },
+        },
+      ],
+    }),
+  ],
+});
